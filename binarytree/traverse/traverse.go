cf026e3db7b07e40aee56005b92ea06f7decfede
wujie <2878354193@qq.com>
2020-05-01 16:54:10 +0800

    BreadthFirstTraverse, DeepFirstTraverse, LevelOrder  traverse

--- binarytree/traverse/traverse.go
+++ binarytree/traverse/traverse.go
@@ -38,3 +38,74 @@ func (node *TreeNode) PostOrderTraverse(){
 	node.RightNode.PostOrderTraverse()
 	PostOrderResult = append(PostOrderResult, node.Value)
 }
+
+// 广度优先遍历
+func (node *TreeNode) BreadthFirstTraverse() []int{
+	var result []int
+	if node == nil {
+		return result
+	}
+	var q []*TreeNode
+	q = append(q, node)
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		result = append(result, node.Value)
+		if node.LeftNode != nil {
+			q = append(q, node.LeftNode)
+		}
+		if node.RightNode != nil {
+			q = append(q, node.RightNode)
+		}
+	}
+	return result
+}
+
+// 深度优先遍历
+func (node *TreeNode) DeepFirstTraverse() []int{
+	var result []int
+	if node == nil {
+		return result
+	}
+	var stack []*TreeNode
+	stack = append(stack, node)
+	for len(stack) > 0 {
+		node := stack[len(stack) - 1]
+		stack = stack[0:len(stack) - 1]
+		result = append(result, node.Value)
+		if node.RightNode != nil {
+			stack = append(stack, node.RightNode)
+		}
+		if node.LeftNode != nil {
+			stack = append(stack, node.LeftNode)
+		}
+	}
+	return result
+}
+
+// 层次遍历(跟广度优先差不多，分层次， 二维数组)
+func (node *TreeNode) LevelOrder() [][]int{
+	result := make([][]int, 0)
+	if node == nil {
+		return result
+	}
+	var q []*TreeNode
+	q = append(q, node)
+	for len(q) > 0 {
+		l := len(q)
+		var arr []int
+		for j := 0; j < l; j++ {
+			node := q[j]
+			arr = append(arr, node.Value)
+			if node.LeftNode != nil {
+				q = append(q, node.LeftNode)
+			}
+			if node.RightNode != nil {
+				q = append(q, node.RightNode)
+			}
+		}
+		result = append(result, arr)
+		q = q[l:]
+	}
+	return result
+}
--- binarytree/traverse/traverse_test.go
+++ binarytree/traverse/traverse_test.go
@@ -1,6 +1,8 @@
 package traverse
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTreeNode_PreOrderTraverse(t *testing.T) {
 	tests := []struct{
@@ -99,5 +101,126 @@ func TestTreeNode_PostOrderTraverse(t *testing.T) {
 			}
 		}
 		PostOrderResult = PostOrderResult[0:0]
+	}
+}
+
+func TestTreeNode_BreadthFirstTraverse(t *testing.T) {
+	tests := []struct{
+		node *TreeNode
+		result []int
+	} {
+		{
+			nil,
+			[]int{},
+		},
+		{
+			&TreeNode{
+				4,
+				&TreeNode{5, nil, &TreeNode{1, nil, nil}},
+				&TreeNode{10, nil, &TreeNode{10, &TreeNode{9, nil, nil}, &TreeNode{20, nil, nil}}},
+			},
+			[]int {4, 5, 10, 1, 10, 9, 20},
+		},
+		{
+			&TreeNode{5, nil, &TreeNode{4, nil, nil}},
+			[]int{5, 4},
+		},
+	}
+	for _, test := range tests {
+		result := test.node.BreadthFirstTraverse()
+		for i, item := range result {
+			if item != test.result[i] {
+				t.Errorf("expect%v, but got %v", test.result, result)
+			}
+		}
+	}
+}
+
+func TestTreeNode_DeepFirstTraverse(t *testing.T) {
+	tests := []struct{
+		node *TreeNode
+		result []int
+	} {
+		{
+			nil,
+			[]int{},
+		},
+		{
+			&TreeNode{
+				4,
+				&TreeNode{5, nil, &TreeNode{1, nil, nil}},
+				&TreeNode{10, nil, &TreeNode{10, &TreeNode{9, nil, nil}, &TreeNode{20, nil, nil}}},
+			},
+			[]int {4, 5, 1, 10, 10, 9, 20},
+		},
+		{
+			&TreeNode{5, nil, &TreeNode{4, nil, nil}},
+			[]int{5, 4},
+		},
+	}
+	for _, test := range tests {
+		result := test.node.DeepFirstTraverse()
+		for i, item := range result {
+			if item != test.result[i] {
+				t.Errorf("expect%v, but got %v", test.result, result)
+			}
+		}
+	}
+}
+
+func TestTreeNode_LevelOrder(t *testing.T) {
+	tests := []struct{
+		node *TreeNode
+		result [][]int
+	} {
+		{
+			nil,
+			[][]int{},
+		},
+		{
+			&TreeNode{
+				4,
+				&TreeNode{5, nil, &TreeNode{1, nil, nil}},
+				&TreeNode{10, nil, &TreeNode{10, &TreeNode{9, nil, nil}, &TreeNode{20, nil, nil}}},
+			},
+			[][]int {
+				{4},
+				{5, 10},
+				{1, 10},
+				{9, 20},
+			},
+		},
+		{
+			&TreeNode{5, &TreeNode{10, nil, nil}, &TreeNode{4, nil, nil}},
+			[][]int {
+				{5},
+				{10, 4},
+			},
+		},
+	}
+	for _, test := range tests {
+		result := test.node.LevelOrder()
+		if len(result) != len(test.result) {
+			t.Errorf("expect%v, but got %v", test.result, result)
+		}
+		for i := 0; i < len(result); i++ {
+			actualItems := result[i]
+			expectItems := test.result[i]
+			if len(actualItems) != len(expectItems) {
+				t.Errorf("expect%v, but got %v", test.result, result)
+				break
+			}
+			success := true
+			for j := 0; j < len(actualItems); j++ {
+				if actualItems[j] != expectItems[j] {
+					t.Errorf("expect%v, but got %v", test.result, result)
+					success = false
+					break
+				}
+			}
+			if !success {
+				break
+			}
+		}
 	}
 }
